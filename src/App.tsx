c6ec5d3fc5d0ffef4c5088be6ed9d42e8e50086d
Tom Marren <tmarren2090@gmail.com>
2022-10-20 15:56:17 -0400

    add icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import "./App.css";
+import github from './github.png'
+import linkedin from './linkedin.png'
 
 function App() {
   return (
@@ -6,19 +8,18 @@ function App() {
       <header className="App-header">
         <h1>Tom Marren</h1>
         <h2 style={{ margin: "0 0 40px 0" }}>Software Engineer</h2>
-        <div>
+        <div style={{display: 'flex', alignItems: 'center'}}>
           <a
-            style={{ marginRight: "20px", color: "white" }}
+            style={{ marginRight: "50px", color: "white" }}
             href="https://github.com/thomasmarren"
           >
-            Github
+            <img src={github} style={{height: '50px', width: '50px'}} alt="github" />
           </a>
-          <span style={{ marginRight: "20px" }}>•</span>
           <a
             style={{ color: "white" }}
             href="https://linkedin.com/in/marrenthomas/"
           >
-            Linkedin
+            <img src={linkedin} style={{height: '50px', width: '50px'}} alt="linkedin" />
           </a>
         </div>
       </header>
