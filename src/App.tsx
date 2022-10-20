1e74aa3e07aaf757ddee54558103bfbfe92b72d6
Tom Marren <tmarren2090@gmail.com>
2022-10-20 15:21:24 -0400

    basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1>Tom Marren</h1>
+        <h1>Tom Marren</h1>
+        <h2 style={{ margin: "0 0 40px 0" }}>Software Engineer</h2>
+        <div>
+          <a
+            style={{ marginRight: "20px", color: "white" }}
+            href="https://github.com/thomasmarren"
+          >
+            Github
+          </a>
+          <span style={{ marginRight: "20px" }}>•</span>
+          <a
+            style={{ color: "white" }}
+            href="https://linkedin.com/in/marrenthomas/"
+          >
+            Linkedin
+          </a>
+        </div>
       </header>
     </div>
   );
