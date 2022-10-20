cb897cc9d5450de4dd58b39be89b7195a613fa8f
Tom Marren <tmarren2090@gmail.com>
2022-10-20 16:00:37 -0400

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
 import "./App.css";
-import github from './github.png'
-import linkedin from './linkedin.png'
+import github from "./github.png";
+import linkedin from "./linkedin.png";
 
 function App() {
   return (
-    <div className="App">
+    <div style={{ textAlign: "center" }}>
       <header className="App-header">
         <h1>Tom Marren</h1>
         <h2 style={{ margin: "0 0 40px 0" }}>Software Engineer</h2>
-        <div style={{display: 'flex', alignItems: 'center'}}>
+        <div style={{ display: "flex", alignItems: "center" }}>
           <a
             style={{ marginRight: "50px", color: "white" }}
             href="https://github.com/thomasmarren"
           >
-            <img src={github} style={{height: '50px', width: '50px'}} alt="github" />
+            <img
+              src={github}
+              style={{ height: "50px", width: "50px" }}
+              alt="github"
+            />
           </a>
           <a
             style={{ color: "white" }}
             href="https://linkedin.com/in/marrenthomas/"
           >
-            <img src={linkedin} style={{height: '50px', width: '50px'}} alt="linkedin" />
+            <img
+              src={linkedin}
+              style={{ height: "50px", width: "50px" }}
+              alt="linkedin"
+            />
           </a>
         </div>
       </header>
